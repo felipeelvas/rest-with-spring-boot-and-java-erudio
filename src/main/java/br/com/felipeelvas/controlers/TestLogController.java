620821568a19d755982fb3361a0722d17ce3cc49
Felipe Elvas Barbosa <elvasfelipe@hotmail.com>
2025-07-25 21:46:28 -0300

    Migrations, migrations com Flyway, via terminal e plugin no POM

--- src/main/java/br/com/felipeelvas/controlers/TestLogController.java
+++ src/main/java/br/com/felipeelvas/controlers/TestLogController.java
@@ -7,7 +7,7 @@ import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
-@RequestMapping("/test")
+@RequestMapping("/api/test/v1")
 public class TestLogController {
 
     private Logger logger = LoggerFactory.getLogger(TestLogController.class.getName());
---
+++ src/main/resources/db/migration/V2__Populate_Table_Person.sql
@@ -0,0 +1,9 @@
+INSERT INTO `person` (`id`, `address`, `first_name`, `gender`, `last_name`) VALUES
+    (1, 'Samambaia _ Brasília _Brasil', 'Felipe', 'Male', 'Elvas'),
+    (2, 'Anchiano _ Italy', 'Leonardo', 'Male', 'da Vinci'),
+    (6, 'Kentucky - US lkkkhjfglkjljhf', 'Muhamamd', 'Male', 'Ali'),
+    (7, 'Kentucky - US', 'Muhamamd', 'Male', 'Ali'),
+    (9, 'Mvezo - South Africa', 'Nelson', 'Male', 'Mandela'),
+    (10, 'Mvezo - South Africa', 'Nelson', 'Male', 'Mandela'),
+    (11, 'Smiljan - Croatia', 'Nikola', 'Male', 'Tesla');
+
